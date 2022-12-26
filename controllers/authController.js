72b3f3e5c28424d51293035da1da8df05a548375
Janishnehyan03 <nehyanjanish@gmail.com>
2022-12-26 19:55:32 +0530

    testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,8 +21,8 @@ const sendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     // httpOnly: true,
-    // secure: process.env.NODE_ENV !== "development",
-    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+    // secure: true,
+    // secure: req.secure || req.headers["x-forwarded-proto"] === "https",
   };
 
   res.cookie("jwt", token, cookieOptions);
