e31c8c861b28abdf8fe7749b3aa685c5f9e0ee97
Janishnehyan03 <nehyanjanish@gmail.com>
2022-12-26 20:08:23 +0530

    secure true added

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,7 @@ const sendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    // secure: true,
+    secure: true,
     // secure: req.secure || req.headers["x-forwarded-proto"] === "https",
   };
 
