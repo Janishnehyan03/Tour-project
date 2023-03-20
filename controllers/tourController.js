f9320993f7196cba6a99ac86dbdb48499c6736af
Janishnehyan03 <nehyanjanish@gmail.com>
2023-03-20 08:24:09 +0530

    edited

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -61,7 +61,7 @@ exports.deleteTours = factory.deleteOne(Tour);
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
   req.query.sort = "-ratingsAverage,price";
-  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty,imageCover";
   next();
 };
 
